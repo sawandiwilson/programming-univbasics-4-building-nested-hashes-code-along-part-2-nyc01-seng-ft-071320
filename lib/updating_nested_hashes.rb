b76961a623319b9f96497a0f61f86c921551a4f3
Sawandi Wilson <sawandiwilson@gmail.com>
2020-07-03 21:17:16 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -1,6 +1,4 @@
 def update_status
-   epic_tragedy = epic_tragedy[:montague][:hero][:status] = "dead"
-   {
       :montague => {
          :patriarch => {name: "Lord Montague", age: "53"},
          :matriarch => {name: "Lady Montague", age: "54"},
